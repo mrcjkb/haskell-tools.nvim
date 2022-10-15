01914a4993cfc02d3645e536343f85b5f4869bf8
Marc Jakobi <marc.jakobi@tiko.energy>
2022-10-15 18:30:04 +0200

    fix: Make on_not_available optional in if_available

--- lua/haskell-tools/deps.lua
+++ lua/haskell-tools/deps.lua
@@ -10,12 +10,15 @@ end
 
 -- @param module name: The name of the module
 -- @param on_available: function(module)
--- @param on_not_available: function() | anything
+-- @param on_not_available: (optonal) function() | anything
 -- @return the return value of on_available or on_not_available
 function M.if_available(modname, on_available, on_not_available)
   local has_mod, mod = pcall(require, modname)
   if has_mod then
     return on_available(mod)
+  end
+  if not on_not_available then
+    return nil
   end
   if type(on_not_available) == 'function' 
     then return on_not_available() 
