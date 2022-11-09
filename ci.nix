c8d98c612af62a88a6e366eff8e432b9c0366946
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-09 19:45:41 +0100

    refactor(nix): formatting

--- ci.nix
+++ ci.nix
@@ -2,7 +2,8 @@ let
   sources = import ./nix/sources.nix;
   pkgs = import ./nix/pkgs.nix { inherit sources; };
   pre-commit = import ./nix/pre-commit.nix { inherit sources; };
-in {
+in
+{
   haskell-tools-test = pkgs.haskell-tools-test;
 
   pre-commit-check = pre-commit.run;
