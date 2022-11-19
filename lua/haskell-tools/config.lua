e48dcd0ac2f74c649dc1be717f0e34b672c845f5
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-19 21:32:00 +0100

    fix(hls): missing settings table in config

--- lua/haskell-tools/config.lua
+++ lua/haskell-tools/config.lua
@@ -1,6 +1,8 @@
 local deps = require('haskell-tools.deps')
 
-local M = {}
+local M = {
+  hls_log = vim.fn.stdpath('data') .. '/' .. 'haskell-language-server.log'
+}
 
 local ht_capabilities = {}
 local cmp_capabilities = deps.if_available(
@@ -82,95 +84,99 @@ local defaults = {
     },
   },
   hls = {
+    debug = false,
     on_attach = function(...) end,
+    cmd = { 'haskell-language-server-wrapper', '--lsp', '--logfile', M.hls_log },
     capabilities = capabilities,
-    haskell = {
-      -- The formatting provider.
-      formattingProvider = 'ormolu',
-      -- Maximum number of completions sent to the LSP client.
-      maxCompletions = 40,
-      -- Whether to typecheck the entire project on initial load.
-      -- Could drive to bad performance in large projects, if set to true.
-      checkProject = true,
-      -- When to typecheck reverse dependencies of a file;
-      -- one of NeverCheck, CheckOnSave (means dependent/parent modules will only be checked when you save),
-      -- or AlwaysCheck (means re-typechecking them on every change).
-      checkParents = 'CheckOnSave',
-      plugin = {
-        alternateNumberFormat = {globalOn = true,},
-        callHierarchy = {globalOn = true,},
-        changeTypeSignature = {globalOn = true,},
-        class = {
-          codeActionsOn = true,
-          codeLensOn = true,
-        },
-        eval = {
-          globalOn = true,
-          config = {
-            diff = true,
-            exception = true,
+    settings = {
+      haskell = {
+        -- The formatting provider.
+        formattingProvider = 'fourmolu',
+        -- Maximum number of completions sent to the LSP client.
+        maxCompletions = 40,
+        -- Whether to typecheck the entire project on initial load.
+        -- Could drive to bad performance in large projects, if set to true.
+        checkProject = true,
+        -- When to typecheck reverse dependencies of a file;
+        -- one of NeverCheck, CheckOnSave (means dependent/parent modules will only be checked when you save),
+        -- or AlwaysCheck (means re-typechecking them on every change).
+        checkParents = 'CheckOnSave',
+        plugin = {
+          alternateNumberFormat = {globalOn = true,},
+          callHierarchy = {globalOn = true,},
+          changeTypeSignature = {globalOn = true,},
+          class = {
+            codeActionsOn = true,
+            codeLensOn = true,
           },
-        },
-        excplicitFixity = {globalOn = true,},
-        gadt = {globalOn = true,},
-        ['ghcide-code-actions-bindings'] = {globalOn = true,},
-        ['ghcide-code-actions-fill-holes'] = {globalOn = true,},
-        ['ghcide-code-actions-imports-exports'] = {globalOn = true,},
-        ['ghcide-code-actions-type-signatures'] = {globalOn = true,},
-        ['ghcide-completions'] = {
-          globalOn = true,
-          config = {
-            autoExtendOn = true,
-            snippetsOn = true,
+          eval = {
+            globalOn = true,
+            config = {
+              diff = true,
+              exception = true,
+            },
           },
-        },
-        ['ghcide-hover-and-symbols'] = {
-          hoverOn = true,
-          symbolsOn = true,
-        },
-        ['ghcide-type-lenses'] = {
-          globalOn = true,
-          config = {
-            mode = 'always',
+          excplicitFixity = {globalOn = true,},
+          gadt = {globalOn = true,},
+          ['ghcide-code-actions-bindings'] = {globalOn = true,},
+          ['ghcide-code-actions-fill-holes'] = {globalOn = true,},
+          ['ghcide-code-actions-imports-exports'] = {globalOn = true,},
+          ['ghcide-code-actions-type-signatures'] = {globalOn = true,},
+          ['ghcide-completions'] = {
+            globalOn = true,
+            config = {
+              autoExtendOn = true,
+              snippetsOn = true,
+            },
           },
-        },
-        haddockComments = {globalOn = true,},
-        hlint = {
-          codeActionsOn = true,
-          diagnosticsOn = true,
-        },
-        importLens = {
-          globalOn = true,
-          codeActionsOn = true,
-          codeLensOn = true,
-        },
-        moduleName = {globalOn = true,},
-        pragmas = {
-          codeActionsOn = true,
-          completionOn = true,
-        },
-        qualifyImportedNames = {globalOn = true,},
-        refineImports = {
-          codeActionsOn = true,
-          codeLensOn = true,
-        },
-        rename = {
-          globalOn = true,
-          config = {crossModule = true,},
-        },
-        retrie = {globalOn = true,},
-        splice = {globalOn = true,},
-        tactics = {
-          codeActionsOn = true,
-          codeLensOn = true,
-          config = {
-            auto_gas = 4,
-            hole_severity = nil,
-            max_use_ctor_actions = 5,
-            proofstate_styling = true,
-            timeout_duration = 2,
+          ['ghcide-hover-and-symbols'] = {
+            hoverOn = true,
+            symbolsOn = true,
           },
-          hoverOn = true,
+          ['ghcide-type-lenses'] = {
+            globalOn = true,
+            config = {
+              mode = 'always',
+            },
+          },
+          haddockComments = {globalOn = true,},
+          hlint = {
+            codeActionsOn = true,
+            diagnosticsOn = true,
+          },
+          importLens = {
+            globalOn = true,
+            codeActionsOn = true,
+            codeLensOn = true,
+          },
+          moduleName = {globalOn = true,},
+          pragmas = {
+            codeActionsOn = true,
+            completionOn = true,
+          },
+          qualifyImportedNames = {globalOn = true,},
+          refineImports = {
+            codeActionsOn = true,
+            codeLensOn = true,
+          },
+          rename = {
+            globalOn = true,
+            config = {crossModule = true,},
+          },
+          retrie = {globalOn = true,},
+          splice = {globalOn = true,},
+          tactics = {
+            codeActionsOn = true,
+            codeLensOn = true,
+            config = {
+              auto_gas = 4,
+              hole_severity = nil,
+              max_use_ctor_actions = 5,
+              proofstate_styling = true,
+              timeout_duration = 2,
+            },
+            hoverOn = true,
+          },
         },
       },
     },
@@ -183,6 +189,9 @@ M.options = {
 
 function M.setup(opts)
   M.options = vim.tbl_deep_extend('force', {}, defaults, opts or {})
+  if M.options.hls.debug then
+    table.insert(M.option.hls.cmd, '--debug')
+  end
 end
 
 return M
