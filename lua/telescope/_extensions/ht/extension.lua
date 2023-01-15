237ee4e4c9c98b032916fc82dc856d314de326f8
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-15 23:14:22 +0100

    docs(lua): add commands to lua docs

--- lua/telescope/_extensions/ht/extension.lua
+++ lua/telescope/_extensions/ht/extension.lua
@@ -1,5 +1,23 @@
 local ht = require('haskell-tools')
 
+--- @brief[[
+--- If `telescope.nvim` is installed, `haskell-tools` will register the `ht` extenstion
+--- with the following commands:
+---
+--- * `:Telescope ht package_files` - Search for files within the current (sub)package.
+--- * `:Telescope ht package_hsfiles` - Search for Haskell files within the current (sub)package.
+--- * `:Telescope ht package_grep` - Live grep within the current (sub)package.
+--- * `:Telescope ht package_hsgrep` - Live grep Haskell files within the current (sub)package.
+--- * `:Telescope ht hoogle_signature` - Run a Hoogle search for the type signature under the cursor.
+---
+--- To load the extension, call
+---
+--- >
+--- require('telescope').load_extension('ht')
+--- <
+---
+--- @brief]]
+
 return {
   exports = {
     -- Live grep within the current (sub)package
