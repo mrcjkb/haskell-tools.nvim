47d30b754753da4b70a18dbfbcec89cf6f067dcd
Marc Jakobi <mrcjkb89@outlook.com>
2023-03-12 10:48:00 +0100

    ci: use nixpkgs-unstable to build neovim-nightly

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
     neovim-nightly-overlay = {
       url = "github:nix-community/neovim-nightly-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
     pre-commit-hooks = {
