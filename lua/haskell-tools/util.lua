bff2e3ee24bb45dabb2e83ab4579f0f3430d44da
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-14 14:42:46 +0200

    fix: prevent nil access

--- lua/haskell-tools/util.lua
+++ lua/haskell-tools/util.lua
@@ -64,13 +64,14 @@ function util.open_browser(url)
 end
 
 --- Pretty-print a type signature
---- @param sig string The raw signature
---- @return string pp_sig The pretty-printed signature
+--- @param sig string|nil The raw signature
+--- @return string|nil pp_sig The pretty-printed signature
 local function pp_signature(sig)
   local pp_sig = sig
-    :gsub('\n', ' ') -- join lines
-    :gsub('forall .*%.%s', '') -- hoogle cannot search for `forall a.`
-    :gsub('^%s*(.-)%s*$', '%1') -- trim
+    and sig
+      :gsub('\n', ' ') -- join lines
+      :gsub('forall .*%.%s', '') -- hoogle cannot search for `forall a.`
+      :gsub('^%s*(.-)%s*$', '%1') -- trim
   return pp_sig
 end
 
@@ -84,8 +85,9 @@ function util.try_get_signatures_from_markdown(func_name, docs)
   ---@type string|nil
   local raw_func_sig = docs:match('```haskell\n' .. func_name .. '%s::%s([^```]*)')
   for code_block in docs:gmatch('```haskell\n([^```]*)\n```') do
+    ---@type string|nil
     local sig = code_block:match('::%s([^```]*)')
-    local pp_sig = pp_signature(sig)
+    local pp_sig = sig and pp_signature(sig)
     if sig and not vim.tbl_contains(all_sigs, pp_sig) then
       table.insert(all_sigs, pp_sig)
     end
