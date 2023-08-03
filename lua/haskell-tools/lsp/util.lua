53014f49f78610629c98ac176964c2ba0fe9d663
Marc Jakobi <marc.jakobi@tiko.energy>
2023-08-03 08:46:22 +0200

    fix: hotfix for neovim-nightly compatibility update

--- lua/haskell-tools/lsp/util.lua
+++ lua/haskell-tools/lsp/util.lua
@@ -17,7 +17,7 @@ util.client_name = 'haskell-tools.nvim'
 
 ---@param bufnr number the buffer to get clients for
 function util.get_active_ht_clients(bufnr)
-  return vim.lsp.get_clients { bufnr = bufnr, name = util.client_name }
+  return util.get_clients { bufnr = bufnr, name = util.client_name }
 end
 
 return util
