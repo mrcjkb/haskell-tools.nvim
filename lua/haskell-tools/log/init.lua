85bcc3129f892cf0cb02432f1bae68fd4216844c
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-02 00:42:56 +0200

    style: log module

--- lua/haskell-tools/log/init.lua
+++ lua/haskell-tools/log/init.lua
@@ -5,25 +5,25 @@ local HaskellToolsLog = {}
 
 ---Get the haskell-language-server log file
 ---@return string filepath
-HaskellToolsLog.get_hls_logfile = function()
+function HaskellToolsLog.get_hls_logfile()
   return require('haskell-tools.log.internal').get_hls_logfile()
 end
 
 ---Get the haskell-tools.nvim log file path.
 ---@return string filepath
-HaskellToolsLog.get_logfile = function()
+function HaskellToolsLog.get_logfile()
   return require('haskell-tools.log.internal').get_logfile()
 end
 
 ---Open the haskell-language-server log file
 ---@return nil
-HaskellToolsLog.nvim_open_hls_logfile = function()
+function HaskellToolsLog.nvim_open_hls_logfile()
   return require('haskell-tools.log.internal').nvim_open_hls_logfile()
 end
 
 ---Open the haskell-tools.nvim log file.
 ---@return nil
-HaskellToolsLog.nvim_open_logfile = function()
+function HaskellToolsLog.nvim_open_logfile()
   return require('haskell-tools.log.internal').nvim_open_logfile()
 end
 
@@ -31,7 +31,7 @@ end
 ---@param level (string|integer) The log level
 ---@return nil
 ---@see vim.log.levels
-HaskellToolsLog.set_level = function(level)
+function HaskellToolsLog.set_level(level)
   return require('haskell-tools.log.internal').set_level(level)
 end
 
