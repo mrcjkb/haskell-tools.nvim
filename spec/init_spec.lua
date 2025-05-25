95ed371dd616125f5f827743b9d0e2ff0f8e275b
Marc Jakobi <marc.jakobi@tiko.energy>
2025-05-25 13:32:12 +0200

    tests: add case for vim.deprecate

---
+++ spec/init_spec.lua
@@ -0,0 +1,28 @@
+local stub = require('luassert.stub')
+describe('Can require haskell-tools with default configs.', function()
+  local notify_once = stub(vim, 'notify_once')
+  local notify = stub(vim, 'notify')
+  local deprecate = stub(vim, 'deprecate')
+  local ht = require('haskell-tools')
+  it('Public API is available after setup.', function()
+    assert(ht.lsp ~= nil)
+    assert(ht.hoogle ~= nil)
+    assert(ht.repl ~= nil)
+    assert(ht.project ~= nil)
+    assert(ht.tags ~= nil)
+  end)
+  it('No notifications at startup.', function()
+    if not pcall(assert.stub(notify_once).called_at_most, 0) then
+      -- fails and outputs arguments
+      assert.stub(notify_once).called_with(nil)
+    end
+    if not pcall(assert.stub(notify).called_at_most, 0) then
+      assert.stub(notify).called_with(nil)
+    end
+  end)
+  it('No deprecation warnings at startup.', function()
+    if not pcall(assert.stub(deprecate).called_at_most, 0) then
+      assert.stub(deprecate).called_with(nil)
+    end
+  end)
+end)
