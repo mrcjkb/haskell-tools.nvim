mrcjkb/haskell-tools.nvim

commit 1516ab3a8564c70b8a9a9c2c92a62dec3f6a8811

fix(lsp): resolve `vim.lsp.config[*]` (#522)
See https://github.com/neovim/neovim/pull/37289#issue-3789030107

lua/haskell-tools/lsp/init.lua
@@ -171,7 +171,12 @@ Hls.start = function(bufnr)
       fix_cabal_client(client)
     end,
   }
-  local client_config = vim.tbl_deep_extend('force', {}, lsp_start_opts, vim.lsp.config['haskell-tools'] or {})
+  local hs_config_name = 'haskell-tools'
+  -- Force resolution of `vim.lsp.config['*']` for `hs_config_name`,
+  -- in case it has not been set
+  -- (This does not overwrite any existing configs).
+  vim.lsp.config(hs_config_name, {})
+  local client_config = vim.tbl_deep_extend('force', {}, lsp_start_opts, vim.lsp.config[hs_config_name] or {})
   local client_id = vim.lsp.start(client_config)
   return client_id
 end

commit 0f66b74e207b3cb1ade73e127df124ed48499207

fix(lsp): client given wrong name [regression] (#527)
Regression introduced in #522.

Fixes #526 (and probably a bunch of other bugs caused by the
regression).

lua/haskell-tools/lsp/init.lua
@@ -51,6 +51,8 @@ end
 
 log.debug('Setting up the LSP client...')
 local hls_opts = HTConfig.hls
+
+---@type table<string, lsp.Handler>
 local handlers = {}
 
 local tools_opts = HTConfig.tools
@@ -171,7 +173,7 @@ Hls.start = function(bufnr)
       fix_cabal_client(client)
     end,
   }
-  local hs_config_name = 'haskell-tools'
+  local hs_config_name = lsp_start_opts.name
   -- Force resolution of `vim.lsp.config['*']` for `hs_config_name`,
   -- in case it has not been set
   -- (This does not overwrite any existing configs).